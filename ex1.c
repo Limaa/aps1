9047869b031f248e75141db1099dbe9dffab267a
Guilherme Lima <guilima10@gmail.com>
2013-07-24 00:42:39 -0300

    ex1 working properly

--- ex1.c
+++ ex1.c
@@ -3,102 +3,107 @@
 
 struct elemento
 {
-	int info;
-	struct elemento *next;
+    int info;
+    struct elemento *next;
 };
 
-void adiciona(struct elemento *inicio, int i)
+void adiciona(struct elemento **lista, int i)
 {
-	struct elemento *novo = (struct elemento *)malloc(sizeof(struct elemento));
-	novo->info = i;
-	novo->next = NULL;
+    struct elemento *novo = (struct elemento *)malloc(sizeof(struct elemento));
+    novo->info = i;
+    novo->next = NULL;
 
-	if(inicio == NULL)
-	{
-		inicio = novo;
-	}
-	else
-	{
-		struct elemento *paux = inicio;
-		while(paux->next != NULL)
-		{
-			paux = paux->next;
-		}
-		paux->next = novo;
-	}
+    if(*lista == NULL)
+    {
+        *lista = novo;
+    }
+    else
+    {
+        struct elemento *paux = *lista;
+        while(paux->next != NULL)
+        {
+            paux = paux->next;
+        }
+        paux->next = novo;
+    }
 }
 
 void crialista(struct elemento **lista)
 {
-	struct elemento *inicio = *lista;
-
-	inicio = NULL;
-	adiciona(inicio, 4);
-	adiciona(inicio, 6);
-	adiciona(inicio, 7);
-	adiciona(inicio, 2);
-	adiciona(inicio, 13);
-	adiciona(inicio, 8);
-	adiciona(inicio, 20);
-	adiciona(inicio, 9);
+    *lista = NULL;
+    adiciona(lista, 4);
+    adiciona(lista, 6);
+    adiciona(lista, 7);
+    adiciona(lista, 2);
+    adiciona(lista, 13);
+    adiciona(lista, 8);
+    adiciona(lista, 20);
+    adiciona(lista, 9);
 }
 
-void clear(struct elemento *inicio)
+void clear(struct elemento **lista)
 {
-	if(inicio != NULL)
-	{
-		struct elemento *paux = inicio;
-		while(paux != NULL)
-		{
-			struct elemento *prox = paux->next;
-			free(paux);
-			paux = prox;
-		}
-	}
-	inicio = NULL;
+    if(*lista != NULL)
+    {
+        struct elemento *paux = *lista;
+        while(paux != NULL)
+        {
+            struct elemento *prox = paux->next;
+            free(paux);
+            paux = prox;
+        }
+    }
+    *lista = NULL;
 }
 
 void imprime(struct elemento *inicio)
 {
-	if(inicio != NULL)
-	{
-		struct elemento *paux = inicio;
-		while(paux != NULL)
-		{
-			printf("%d\n", paux->info);
-			paux = paux->next;
-		}
-	}
+    if(inicio != NULL)
+    {
+        struct elemento *paux = inicio;
+        while(paux != NULL)
+        {
+            printf("%d\n", paux->info);
+            paux = paux->next;
+        }
+        printf("-------------\n");
+    }
 }
 
 void separa(struct elemento *source, struct elemento **listaPar, struct elemento **listaImpar)
 {
-	struct elemento *par = *listaPar;
-	struct elemento *impar = *listaImpar;
+    *listaPar = NULL;
+    *listaImpar = NULL;
 
-	if(source != NULL)
-	{
-		struct elemento *paux = source;
-		while(paux != NULL)
-		{
-			if((paux->info)%2 == 0)
-				adiciona(par, paux->info);
-			else
-				adiciona(impar, paux->info);
+    if(source != NULL)
+    {
+        struct elemento *paux = source;
+        while(paux != NULL)
+        {
+            if((paux->info)%2 == 0)
+                adiciona(listaPar, paux->info);
+            else
+                adiciona(listaImpar, paux->info);
 
-			paux = paux->next;
-		}
-	}
+            paux = paux->next;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	struct elemento *A, *PAR, *IMPAR;
-	crialista(&A);
-	separa(A, &PAR, &IMPAR);
-	imprime(A);
-	imprime(PAR);
-	imprime(IMPAR);
-	
-	return 0;
+    struct elemento *A, *PAR, *IMPAR;
+    crialista(&A);
+    separa(A, &PAR, &IMPAR);
+    imprime(A);
+    imprime(PAR);
+    imprime(IMPAR);
+
+	clear(&A);
+	clear(&PAR);
+	clear(&IMPAR);
+
+    return 0;
 }
+
+
